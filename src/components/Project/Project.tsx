e5ae3e481b4e6b62780c9a3e1d04fc6e288ceb50
HaiderBhattiJKD <bhattijkd@gmail.com>
2025-11-22 06:11:07 -0800

    Deploy Updated Build

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -5,7 +5,10 @@ import ScrollAnimation from "react-animate-on-scroll";
 import rider3dIcon from "../../assets/Rider3D.png"
 import bikeStuntIcon from "../../assets/BikeStunt.png"
 import urbanBusIcon from "../../assets/UBS.png"
-import cityGangsterIcon from "../../assets/CityGangster.png"
+import springLancerIcon from "../../assets/Lancer.png"
+import bikeRacingMasterIcon from "../../assets/BRM.png"
+import formulaRacingIcon from "../../assets/F1Racing.png"
+import animalBikeStuntIcon from "../../assets/AnimalBikeStunt.png"
 
 export function Project() {
   return (
@@ -17,7 +20,7 @@ export function Project() {
           <div className="project"> 
             <header>
               <div className="project-links" >
-                <a href="https://github.com/CodeVinayak/Serverless-Voting-Application" target="_blank" rel="noreferrer">
+                <a href="https://play.google.com/store/apps/details?id=com.ffgames.motoercycle.traffic.racer.bikegames.rider.motobikeracing3d&hl=en" target="_blank" rel="noreferrer">
                   <img src={playStoreIcon} alt="Visit site" /></a>
                  </div>
             </header>
@@ -37,7 +40,7 @@ export function Project() {
             <header>
               
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/AI-Chatbot-Assistant" target="_blank" rel="noreferrer">
+                <a href="https://play.google.com/store/apps/details?id=com.kn.bikestunt3.racing.driving.games&hl=en" target="_blank" rel="noreferrer">
                   <img src={playStoreIcon} alt="Visit site" /> </a>
                 
               </div>
@@ -61,7 +64,7 @@ export function Project() {
             <header>
               
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/Jupyter-Notebook/tree/main/Credit%20Risk%20Analysis" target="\_blank" rel="noreferrer">
+                <a href="https://play.google.com/store/apps/details?id=com.ffg.offline.games.driving.city.bus.simulator.busgames&hl=en" target="\_blank" rel="noreferrer">
                   <img src={playStoreIcon} alt="Visit site" />
                 </a>
                 
@@ -88,15 +91,15 @@ export function Project() {
             <header>
               
               <div className="project-links">
-                <a href="https://github.com/CodeVinayak/MediBook-Medical-Appointment-Scheduler-with-Health-History" target="_blank" rel="noreferrer">
+                <a href="https://play.google.com/store/apps/details?id=com.tf.springbolt.smash&hl=en" target="_blank" rel="noreferrer">
                   <img src={playStoreIcon} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body"style={{ textAlign: "center" }}>
-            <img src={cityGangsterIcon} style={{ alignItems:"center", height: '100px' }} />
-              <h3>City Gangster</h3>
-              <p> City Gangster - Dominate the urban jungle, complete high-stakes heists, and outsmart rival gangs as you carve your path to power in this thrilling open-world crime adventure! </p>
+            <img src={springLancerIcon} style={{ alignItems:"center", height: '100px' }} />
+              <h3>Lancer Strike</h3>
+              <p> Lancer Strike - Aim, throw, and smash your way through enemies in this fast-paced spear-flinging action game! Master precision shots, break obstacles, and conquer challenging levels as you rise to become the ultimate lancer warrior. </p>
             </div>
             <footer>
               <ul className="tech-list">
@@ -106,6 +109,78 @@ export function Project() {
             </footer>
           </div>
         </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project" >
+            <header>
+              
+              <div className="project-links">
+                <a href="https://play.google.com/store/apps/details?id=com.tf.ride.master.bike.race.game&hl=en" target="_blank" rel="noreferrer">
+                  <img src={playStoreIcon} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body"style={{ textAlign: "center" }}>
+            <img src={bikeRacingMasterIcon} style={{ alignItems:"center", height: '100px' }} />
+              <h3>Bike Racing Master</h3>
+              <p> Bike Racing Master - Hit the throttle, soar off ramps, dodge wild obstacles and race your way to victory! Unlock high-performance bikes, master challenging tracks and feel the rush of speed in this addictive stunt-racing adventure. </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Unity 3D</li>
+                <li>C#</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project" >
+            <header>
+              
+              <div className="project-links">
+                <a href="https://play.google.com/store/apps/details?id=com.tf.formularacing.pro.drivinggame&hl=en" target="_blank" rel="noreferrer">
+                  <img src={playStoreIcon} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body"style={{ textAlign: "center" }}>
+            <img src={formulaRacingIcon} style={{ alignItems:"center", height: '100px' }} />
+              <h3>Formula Car Racing Pro</h3>
+              <p> Formula Racing Pro - Strap in, hit the throttle, and dominate the asphalt in this adrenaline-charged open–wheel racing adventure! Pilot ultra-fast formula machines, navigate sharp corners at break-neck speed, and race past rivals to become the ultimate champion on the world’s deadliest circuits. </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Unity 3D</li>
+                <li>C#</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>  
+        
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project" >
+            <header>
+              
+              <div className="project-links">
+                <a href="https://play.google.com/store/apps/details?id=com.frolics.bike.stunt.racinggames&hl=en" target="_blank" rel="noreferrer">
+                  <img src={playStoreIcon} alt="Visit site" />
+                </a>
+              </div>
+            </header>
+            <div className="body"style={{ textAlign: "center" }}>
+            <img src={animalBikeStuntIcon} style={{ alignItems:"center", height: '100px' }} />
+              <h3>Animal Bike Stunt</h3>
+              <p> Animal Bike Stunt Racing - Take on wild stunt tracks as you ride powerful animal-themed bikes! Perform crazy flips, soar over extreme ramps, and conquer challenging obstacle courses in this adrenaline-packed bike stunt adventure. Master each animal bike and prove yourself as the ultimate stunt rider! </p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>Unity 3D</li>
+                <li>C#</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>  
 
       </div>
     </Container>
